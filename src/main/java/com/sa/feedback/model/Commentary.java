f29800b642e49d0bea252301eb90368f8b99632c
baangaritar <baangaritar@unal.edu.co>
2019-07-01 13:39:47 -0500

    Other ways

--- src/main/java/com/sa/feedback/model/Commentary.java
+++ src/main/java/com/sa/feedback/model/Commentary.java
@@ -25,13 +25,13 @@ public class Commentary {
     @Column(name="subject", nullable = false)
     private String subject;
 
-    @Column(name="createdAt", nullable = false)
+    @Column(name="created_at", nullable = false)
     private Date createdAt;
 
-    @Column(name="updatedAt", nullable = false)
+    @Column(name="updated_at", nullable = false)
     private Date updatedAt;
 
-    @Column(name="idVideo", nullable=false) 
+    @Column(name="id_video", nullable=false) 
     private String idVideo;
     
     @Column(name="likes", nullable = true) 
@@ -40,7 +40,7 @@ public class Commentary {
     @Column(name="description", nullable=false)
     private String description;
 
-    @Column(name="idUser", nullable=false)
+    @Column(name="id_user", nullable=false)
     private Integer idUser;
 
 
